5dbb77138c7e6cd04111fbce0ae416ae37bdae25
andrés <olivera.h.andres@gmail.com>
2023-07-28 13:52:46 -0300

    Creación de login.html y register.html & retoques del footer

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')));
 app.get('/carrito',(req,res)=>res.sendFile(path.join(__dirname,'views','carrito.html')));
-app.get('/formulario',(req,res)=>res.sendFile(path.join(__dirname,'views','formulario.html')));
+app.get('/register',(req,res)=>res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/registerOk',(req,res)=>res.sendFile(path.join(__dirname,'views','registerOk.html')));
 app.get('/login',(req,res)=>res.sendFile(path.join(__dirname,'views','login.html')));
 app.get('/productos',(req,res)=>res.sendFile(path.join(__dirname,'views','productos.html')))
 app.get('/*',(req,res)=>res.sendFile(path.join(__dirname,'views','404.html')));

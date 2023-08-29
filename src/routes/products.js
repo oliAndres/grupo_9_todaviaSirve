58708ab78f7e6808123f29dafa2e833a5637bb62
meligaunas <gaunasmelitza@gmail.com>
2023-08-29 20:39:39 -0300

    Create

--- src/routes/products.js
+++ src/routes/products.js
@@ -7,5 +7,5 @@ router.get('/detail/:id', productsController.detail);
 router.get('/edit', productsController.edit); 
 
 router.get('/add', productsController.add);
-
+router.post('/add', productsController.create)
 module.exports = router;

09b84c0a8161f7af41c6c08e12e537d7c3f6d49d
federico Chandia <fedelchandia@gmail.com>
2023-09-17 20:30:04 -0300

    feat: login

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 const loginValidator = require("../validations/loginValidator");
-const checkInvited = require('../middlewares/checkInvited');
 const loginProcess = require('../controllers/users/loginProcess');
-const {arrayValidaciones,validateCreateForm } = require('../middlewares/validacionesRegister')
+const {arrayValidaciones,validateCreateForm } = require('../middlewares/validacionesRegister');
+const userCheck = require('../middlewares/userCheck');
+const notUserCheck = require('../middlewares/notUserCheck');
 
 /* GET users listing. */
 router.get("/register", usersController.register)
-router.get("/login", checkInvited, usersController.login)
+router.get("/login", notUserCheck, usersController.login)
 router.post("/login", loginProcess, usersController.loginProcess)
 router.get('/register', usersController.register);
 router.post('/registerOk', arrayValidaciones,validateCreateForm,usersController.newUser);

ca153732aeab5a48233c0bedff43cf48a6715e0d
federico Chandia <fedelchandia@gmail.com>
2023-09-17 21:19:20 -0300

    fix: login

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,7 +10,7 @@ const notUserCheck = require('../middlewares/notUserCheck');
 /* GET users listing. */
 router.get("/register", usersController.register)
 router.get("/login", notUserCheck, usersController.login)
-router.post("/login", loginProcess, usersController.loginProcess)
+router.post("/login", loginValidator, usersController.loginProcess)
 router.get('/register', usersController.register);
 router.post('/registerOk', arrayValidaciones,validateCreateForm,usersController.newUser);
 router.get('/logOut', usersController.logOut);

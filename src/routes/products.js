0a72a40f3ee44ce57cafbf3cc9a076776cde05ae
andrés <olivera.h.andres@gmail.com>
2023-09-27 14:36:17 -0300

    agrega: adminCheck, en la ruta: edit

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,11 +2,12 @@ const express = require('express');
 const productsController = require('../controllers/productsController');
 const upload = require('../middlewares/upload');
 const userCheck = require('../middlewares/userCheck');
+const adminCheck = require('../middlewares/adminCheck');
 const router = express.Router();
 
 router.get('/detail/:id', productsController.detail);
 
-router.get('/edit/:id', productsController.edit);
+router.get('/edit/:id',adminCheck, productsController.edit);
 router.put('/update/:id',upload.array('images'), productsController.update);
 
 router.get('/add',userCheck, productsController.add);

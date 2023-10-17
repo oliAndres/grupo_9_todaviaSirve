471a934bf457bd208a4ef01541c168d3674698ce
meligaunas <gaunasmelitza@gmail.com>
2023-10-17 17:19:01 -0300

    Implementación de sequelize mysql2 dotenv

---
+++ src/database/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+module.exports= {
+  "development": {
+    "username": process.env.DB_USERNAME_DEV,
+    "password": process.env.DB_PASSWORD_DEV,
+    "database": process.env.DB_DATABASE_DEV,
+    "host": process.env.DB_HOST_DEV,
+    "port": process.env.DB_PORT_DEV,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}

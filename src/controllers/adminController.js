7d7359ca864e5112dc3e70eda70dfa09518f2537
andrés <olivera.h.andres@gmail.com>
2023-10-25 01:04:36 -0300

    modifica edit & update

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,12 +1,22 @@
-
-const products= require('../data/products.json');
-
+const db = require('../database/models')
 
 module.exports = {
     admin : (req,res) => {
-     res.render('admin',{
-        products:products
-     });
+
+        const products = db.Product.findAll();
+        const categories = db.Category.findAll();
+        const users = db.User.findAll();
+
+        Promise.all([products,categories,users])
+            .then(([products,categories,users]) => {
+                return res.render('admin',{
+                    products,
+                    categories,
+                    users
+                });
+            })
+            .catch(error => console.log(error))
+        
     }
  
 }

5e6a9aea4c013b4eef618f73103a3d8491ab8b1f
Eric <menaericdaniel@gmail.com>
2023-11-08 11:48:51 -0300

    fix: view products in home

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,13 +1,16 @@
 const db = require('../database/models')
 const products= require('../data/products.json');
+const product = require('../database/models/product');
 
 module.exports = {
     index : (req,res) => {
         console.log(req.session.userLogin)
 
-        db.Product.findAll()
+        db.Product.findAll({
+            include : ['images']
+        })
             .then(products => {
-                res.render('admin.ejs', {products})
+                return res.render('index', {products})
             }).catch(error => console.log(error))
         
         

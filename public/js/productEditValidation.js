18de7994889bbebbb0415923403895951564bc32
meligaunas <gaunasmelitza@gmail.com>
2023-11-20 15:35:26 -0300

    Validación Frontend productEdit

--- public/js/productEditValidation.js
+++ public/js/productEditValidation.js
@@ -1,53 +1,63 @@
-console.log("Hello");
 const $ = (id) => document.getElementById(id);
+
+const show = (info, error, element) => {
+  $(info).hidden = false
+  $(error).hidden = true
+  element.borderColor = 'inherit'
+}
+
+const validateElement = (info, error, element) => {
+  $(info).hidden = true
+  if(!element.target.value.trim()){
+      $(error).hidden = false
+  }
+}
 
 window.onload = function () {
   const main = document.querySelector("main");
 
   const formName = document.querySelector("#name");
-
-  //console.log(formName)
-
   const formBrand = document.querySelector("#brandId");
-  //console.log(brandId)
-
-  //const formDescrption = document.querySelector('#description')
-  //console.log(description)
-
-  //const form = document.querySelector('#price')
-  //console.log(price)
-
-  //const formCategory = document.getElementById('#categoryId')
-  //console.log(categoryId);
-
-  // esta va
-  //console.log($('price'))
-
   const respuesta = confirm("Deseas editar un producto?");
-  //captura el nombre
-
   const formEdit = document.querySelector(".editProduct__main__form");
 
-  //obtengo los elementos del formulario Y LOS LLAMO POR SU POSICIÓN
+  
   let elementsForm = formEdit.elements;
-  console.log(elementsForm);
 
-  /* ME MUESTRA LO Q SE VA ESRIBIENDO EN LA CONSOLA */
+  
+  /* MUESTRA LO Q SE VA ESRIBIENDO EN LA CONSOLA */
   elementsForm[0].addEventListener("keydown", (e) => {
     console.log(e.key);
   });
- //$("name").focus();
+ $("name").focus();
+ 
 
-  $("name").addEventListener("focus", () => {
-    $("msg-name").hidden = false
-    $('error-name').hidden = true
+  $("name").addEventListener("focus", (e) => {
+    show('msg-name', 'error-name', e)
+  })
+
+  $("name").addEventListener("blur", function (e) {
+    validateElement('msg-name', 'error-name', e)
+  })
+
+
+  $("price").addEventListener("focus", (e) => {
+    show('msg-price', 'error-price', e)
   });
 
-  $("name").addEventListener("blur", function () {
-    $("msg-name").hidden = true
-    if(!this.value.trim()){
-        $('error-name').hidden = false
-    }
+  $("price").addEventListener("blur", function (e) {
+    validateElement('msg-price', 'error-price', e)
+  });
+
+  $("description").addEventListener("focus", (e) => {
+    show('msg-description', 'error-description', e)
+  });
+
+  $("description").addEventListener("blur", function (e) {
+    validateElement('msg-description', 'error-description', e)
+  });
+  $("categoryId").addEventListener("focus", (e) => {
+    show('msg-category', 'error-category', e)
   });
 
   formEdit.addEventListener("submit", (event) => {
@@ -58,17 +68,16 @@ window.onload = function () {
     for (let i = 0; i < elementsForm.length; i++) {
       if (elementsForm[i].value.trim() === "") {
         msgErrors.push(`El campo ${elementsForm[i].name} no puede estar vacío`);
+        elementsForm[i].style.borderColor = 'red'
+      }else{
+        elementsForm[i].style.borderColor = 'inherit'
       }
     }
-    //console.log(msgErrors)
-    /*--- OJO  no se muestra listado de errores--*/
-    const boxErrors = $("box-errors");
-    boxErrors.innerHTML = ""; // Limpiar errores anteriores
-
-    if (msgErrors.length) {
-      msgErrors.forEach((msg) => {
-        boxErrors.innerHTML += `<li style="color: red">${msg}</li>`;
-      });
+    
+    if(msgErrors.length) {
+      alert('Los campos señalados son obligatorios')
+    }else {
+      this.submit()
     }
   })
 }

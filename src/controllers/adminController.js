296545fae8a9dd2a6121f22b10afaa8d96f0d005
federico Chandia <fedelchandia@gmail.com>
2023-11-24 16:35:42 -0300

    fix: admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,13 +1,24 @@
-const db = require('../database/models');
+const db = require("../database/models");
 
 module.exports = {
-    admin: (req, res) => {
-        db.Product.findAll({
-            include: ['category', 'section', 'images'],
-        })
-            .then(products => {
-                res.render('admin', { products });
-            })
-            .catch(error => console.log(error));
-    },
+    admin: async (req, res) => {
+        try {
+            const products = await db.Product.findAll({
+                include: [
+                    {
+                        model: db.Brand,
+                        as: 'brand',
+                    },
+                    {
+                        model: db.Category,
+                        as: 'category',
+                    }
+                ]
+            });
+            res.render('admin', { products });
+        } catch (error) {
+            console.error('Error al recuperar productos:', error);
+            res.status(500).json({ error: 'Error interno del servidor' });
+        }
+    }
 };

4543248fd3ca15f12b6e7119620b2f51cc26f18f
federico Chandia <fedelchandia@gmail.com>
2023-12-07 20:39:21 -0300

    fix: views & details

--- public/js/searchBarValidator.js
+++ public/js/searchBarValidator.js
@@ -3,24 +3,24 @@ const regex = /^[a-zA-Z0-9]+$/;
 
 $('search').addEventListener('click', function (event) {
     if (!this.value.trim()) {
-        $('searchBar').placeholder = 'Su busqueda está vacia';
+        $('searchBar').placeholder = 'Su búsqueda está vacía';
         event.preventDefault();
-    }else{
-        event.submit()
+    } else {
+        this.form.submit(); 
     }
-})
+});
+
 
 $('search').addEventListener('blur', function (event) {
     $('searchBar').placeholder = '¿Qué productos buscas....?';
-})
+});
 
-// $('searchBar').addEventListener('input', function (event) {
-//     if (!regex.test(this.value)) {
-//         $('msgError').textContent = 'Solo se permiten caracteres alfanuméricos';
-//         event.preventDefault();
-//     }else {
-//         $('msgError').hidden
-//     }
-// })
-
-
+$('searchBar').addEventListener('change', function (event) {
+    if (!regex.test(this.value)) {
+        event.preventDefault();
+    } else {
+        searchTimeout = setTimeout(() => {
+        this.form.submit(); 
+    }, 750);
+    }
+});

1161653b595e4b790f1e9c4202bf79096d50156c
meligaunas <gaunasmelitza@gmail.com>
2023-12-15 10:21:25 -0300

    Iniciando crud categ

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,6 +1,14 @@
 const db = require('../database/models')
-module.exports ={
+/*module.exports ={
     category : (req,res) => {
         return res.render('category');
     }
+}*/
+module.exports= { 
+add : require('./categories/add'),
+create : require('./categories/create')
 }
+
+
+
+
--- src/routes/category.js
+++ src/routes/category.js
@@ -3,6 +3,23 @@ const categoryController = require('../controllers/categoryController');
 
 const router = express.Router();
 
-router.get('/', categoryController.category);
+router.get('/', categoryController.add);
+router.post('/add', categoryController.create)
+
+
+
+
+ 
+  //Rutas exigidas para la creación del CRUD
+  /*.get("/category", categoryController.add);*/
+  /*.post("/category/create", upload.single('image'), create)
+ 
+  .put("/category/update/:id", upload.single('image'), update)
+ 
+  .delete("/category/delete/:id", destroy)*/
+  
+
+ 
+
 
 module.exports = router;

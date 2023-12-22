8a5704f99ba1372ccdc5f2b8608b08e9d94d7d80
meligaunas <gaunasmelitza@gmail.com>
2023-12-21 22:22:46 -0300

    Fix delete brand

--- src/routes/brand.js
+++ src/routes/brand.js
@@ -10,6 +10,6 @@ router.post('/add',uploadBrand.single('image'),brandController.create);
 router.get('/edit/:id', brandController.edit)
 router.put('/update/:id',uploadBrand.single('image'), brandController.update)
 
-router.delete('/remote/:id',brandController.delete)
+router.delete('/remove/:id',brandController.delete)
 
 module.exports = router;
